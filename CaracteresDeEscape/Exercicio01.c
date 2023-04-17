e893f901681a79d8834dbf12da22a8686d76f14b
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-04-17 15:17:02 -0300

    Create Exercicio01.c

---
+++ CaracteresDeEscape/Exercicio01.c
@@ -0,0 +1,18 @@
+/*Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código 
+com a Linguagem C
+professormarcosp@gmail.com
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	printf("\n\t\"Exercício Sobre Caracteres de Escape\"");
+	system("Pause > Nul");
+	
+	return 0;
+}
