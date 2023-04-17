a2bd1460cd02fef7e8faccff1a442c8e56407731
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-04-17 15:29:57 -0300

    Create Exercicio02.c

---
+++ CaracteresDeEscape/Exercicio02.c
@@ -0,0 +1,18 @@
+/*Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código 
+com a Linguagem C
+professormarcosp@gmail.com
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "Portuguese");
+	printf("\n\n\n\t\t\t\"Como colocar Apóstrofo \' Aspas duplas \" e Barra invertida \\ na tela?\"");
+	system("Pause > Nul");
+	
+	return 0;
+}
