79a6cf7c6c1704c04db321f16850b520f0db422f
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-04-18 16:35:48 -0300

    Create ExercicioDelay.c

---
+++ ExercicioResolvidoDelay/ExercicioDelay.c
@@ -0,0 +1,24 @@
+//Professor Marcos Pacheco
+//professormarcosp@gmail.com
+//Curso: Aprendendo Programação do Zero ao Código com a Linguagem C 
+
+/*Crie um programa em C, que coloque na tela o número 1234
+e depois e 4 segundos retroceda 4 caracteres com 4  \b 
+e em seguida espere 4 segundos e escreva 4 caracteres 
+espaços para apagar da tela o número 1234*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+	printf("\n\t\t1234");
+	Sleep(4000);
+	printf("\b\b\b\b");
+	Sleep(4000);
+	printf("    ");
+	system("Pause > Nul");
+		
+	return 0;
+}
