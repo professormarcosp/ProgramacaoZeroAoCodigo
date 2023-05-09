5c47671e8f7bdfaa283f0f8cd77d534a7e0c181c
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-05-09 17:59:50 -0300

    Create ExCaracteresEscapeMacOSXcode01.c

---
+++ CaracteresDeEscape/MacOSXcode/ExCaracteresEscapeMacOSXcode01.c
@@ -0,0 +1,20 @@
+/*Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código 
+com a Linguagem C
+professormarcosp@gmail.com
+*/
+
+//Professor Marcos Pacheco
+/*Criando e Compilando Programa em C com Xcode no macOS*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+   printf("\n\t\"Exercício Sobre Caracteres de Escape\"\n");
+   system("read -n 1 -s -p \"Pressione Qualquer Tecla para Continuar\"");
+   
+   return 0;
+
+}
