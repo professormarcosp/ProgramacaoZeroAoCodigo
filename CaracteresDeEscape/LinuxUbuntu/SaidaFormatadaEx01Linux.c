cb735bca935685139dbf4e26b867a5ca081658bd
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-05-09 16:46:13 -0300

    Create SaidaFormatadaEx01Linux.c

---
+++ CaracteresDeEscape/LinuxUbuntu/SaidaFormatadaEx01Linux.c
@@ -0,0 +1,18 @@
+/*Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código 
+com a Linguagem C
+professormarcosp@gmail.com
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	system("clear");
+	printf("\n\t\"Exercício Sobre Caracteres? de Escape\"");
+	printf("\n\tPressione qualquer tecla para continuar...\n");
+	getchar();
+	
+	return 0;
+}
