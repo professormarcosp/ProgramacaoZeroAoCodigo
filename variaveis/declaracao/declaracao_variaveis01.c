5d8cad4259a8a2deb6a1fd58c21f5828f84f2edf
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-05-29 16:57:33 -0300

    Create declaracao_variaveis01.c

---
+++ variaveis/declaracao/declaracao_variaveis01.c
@@ -0,0 +1,29 @@
+/*Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+professormarcosp@gmail.com
+*/
+
+/*Crie um programa em linguagem C que declara as seguintes variáveis:
+Uma variável do tipo inteiro de nome numero
+Uma variável do tipo float de nome salario
+Uma variável do tipo double de nome consumo_total
+Uma variável do tipo booleano de nome sinal_ativado*/
+
+#include <stdio.h>
+
+
+int main()
+{
+   int numero;  //int número inteiros
+   float salario;  // float números ponto flutuantes ou números reais com vírgula (.ponto)
+   double consumo_total;// double, é um float com maior precisão, com dupla precisão
+   //consegue armazenar números bem maiores, com maiores dígitos
+   _Bool sinal_ativado; // booleano estados verdadeiro (true) e falso (false)
+   //sendo que 0(zero) é falso e 1(um)é considerado verdadeiro em C
+   //na realidade 0 é falso e qualquer coisa diferente de zero é considerado verdadeiro em C
+
+   getchar();
+   return 0;
+}
