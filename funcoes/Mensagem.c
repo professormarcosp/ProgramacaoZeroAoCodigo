57f351c7679e7c15b736a19b2f102f303a2b0338
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-07-26 09:58:09 -0300

    Create Mensagem.c

---
+++ funcoes/Mensagem.c
@@ -0,0 +1,24 @@
+/*Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+professormarcosp@gmail.com
+*/
+#include <stdio.h>
+
+//Declaração da função ou Protótipo da Função
+void mensagem();
+
+int main()
+{
+	mensagem(); //chamada, invocação da função
+
+	return 0;
+
+}
+
+//Definição da Função
+void mensagem()
+{
+	printf("Mensagem");
+}
