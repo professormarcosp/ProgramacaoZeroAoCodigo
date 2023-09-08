4b9191f2d5acd3b581a887a3ac5b5611ecb571d3
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-09-08 16:16:13 -0300

    Create CodigoInicialExercicioProposto.c

---
+++ scanf/CodigoInicialExercicioProposto.c
@@ -0,0 +1,17 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+
+#include <stdio.h>
+
+int main() {
+    int valor1 = 5;
+    int valor2 = 7;
+    int resultado = valor1 + valor2;
+
+    printf("A soma de %d e %d é %d\n", valor1, valor2, resultado);
+
+    return 0;
+}
