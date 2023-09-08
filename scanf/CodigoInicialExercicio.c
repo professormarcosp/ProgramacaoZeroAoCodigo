9ee240d76840e568b9a400b9245503fe7fb97b86
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-09-08 15:11:56 -0300

    Create CodigoInicialExercicio.c

---
+++ scanf/CodigoInicialExercicio.c
@@ -0,0 +1,19 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+/*Exercício: Modifique o programa abaixo para que agora os dados possam 
+ser solicitados ao usuário utilizando a função scanf*/
+
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 7;
+    int resultado = num1 + num2;
+
+    printf("A soma de %d e %d é %d\n", num1, num2, resultado);
+
+    return 0;
+}
