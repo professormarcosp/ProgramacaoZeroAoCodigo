eec657d592c14ad2819626ab7f626132ac7a45b0
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-09-11 17:02:18 -0300

    Create CodigoCom3Scanfs.c

---
+++ scanf/CodigoCom3Scanfs.c
@@ -0,0 +1,29 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+#include <stdio.h>
+
+int main()
+{
+   char nome[50];
+   int idade;
+   float salario;
+
+   printf("Digite seu nome: ");
+   scanf("%s", nome);
+   //string não colocar & na frente do nome da variavel!
+
+   printf("Digite sua idade: ");
+   scanf("%d", &idade);
+
+   printf("Digite seu salario ");
+   scanf("%f", &salario);
+
+   printf("Nome: %s\n", nome);
+   printf("Idade: %d\n", idade);
+   printf("Salario: %.2f\n", salario);
+
+   return 0;
+}
