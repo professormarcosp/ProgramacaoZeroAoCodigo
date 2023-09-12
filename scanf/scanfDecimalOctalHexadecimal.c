8f2ec3c3c6ef3e0498d485f1eafdb6e2fe4668df
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-09-12 10:13:40 -0300

    Create scanfDecimalOctalHexadecimal.c

---
+++ scanf/scanfDecimalOctalHexadecimal.c
@@ -0,0 +1,31 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+
+#include <stdio.h>
+
+int main() {
+    int decimal;
+    int octal;
+    int hexadecimal;
+
+    printf("Digite um numero decimal: ");
+    scanf("%i", &decimal);//base10
+    fflush(stdin);
+
+    printf("Digite um numero octal: ");
+    scanf("%i", &octal); //base8  colocar 0(zero) na frente do numero octal
+    fflush(stdin);
+
+    printf("Digite um numero hexadecimal: ");
+    scanf("%i", &hexadecimal); //base16 colocar 0x na frente do numero hexadecimal
+    fflush(stdin);
+
+    printf("Valor decimal: %i\n", decimal);
+    printf("Valor octal: %i\n", octal);
+    printf("Valor hexadecimal: %i\n", hexadecimal);
+
+    return 0;
+}
