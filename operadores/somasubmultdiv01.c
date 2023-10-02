efdf2207642818aa864e07da96e9df9849315785
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-10-02 16:18:00 -0300

    Create somasubmultdiv01.c

---
+++ operadores/somasubmultdiv01.c
@@ -0,0 +1,29 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+   float num1, num2;
+   float soma, subtracao, multiplicacao, divisao;
+
+   printf("\nDigite o primeiro numero: "); scanf("%f", &num1);
+   printf("\nDigite o segundo numero: "); scanf("%f", &num2);
+
+   soma = num1 + num2;
+   subtracao = num1 - num2;
+   multiplicacao = num1 * num2;
+   divisao = num1 / num2;
+
+   printf("\n%2.f + %.2f = %.2f", num1, num2, soma);
+   printf("\n%2.f - %.2f = %.2f", num1, num2, subtracao);
+   printf("\n%2.f x %.2f = %.2f", num1, num2, multiplicacao);
+   printf("\n%2.f / %.2f = %.2f", num1, num2, divisao);
+
+   return 0;
+}
