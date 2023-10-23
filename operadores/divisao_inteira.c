3b32c233398921bd98a75942b52af062826da5d1
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-10-23 15:25:33 -0300

    Create divisao_inteira.c

---
+++ operadores/divisao_inteira.c
@@ -0,0 +1,16 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+   double resultado = 7 / 2;
+   printf("%d",resultado);
+
+   return 0;
+}
