30e2042198201462477b2b8b8a6a286b31d60a9f
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-10-27 15:40:13 -0300

    Create exercicio04.c

---
+++ operadores/simplificados/exercicio04.c
@@ -0,0 +1,19 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+   int d = 50;
+
+   d /= 10; // o mesmo que: d = d / 50; d /= 50;
+
+   printf("O valor de d eh: %d\n", d);
+
+   return 0;
+}
