193f117118b2535de06d240bf80d486b76b72fe6
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-10-27 15:27:31 -0300

    Create exercicio01.c

---
+++ operadores/simplificados/exercicio01.c
@@ -0,0 +1,20 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+   int a = 10;
+
+   a += 5; // o mesmo que:
+   //a = a + 5; a += 5
+
+   printf("O valor de a eh: %d\n", a);
+
+   return 0;
+}
