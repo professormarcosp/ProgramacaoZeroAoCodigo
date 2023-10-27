e6cbe8dfbb73cdf87fa3e1d895417c0a5304ec82
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-10-27 15:31:16 -0300

    Create exercicio02.c

---
+++ operadores/simplificados/exercicio02.c
@@ -0,0 +1,20 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+   int b = 20;
+
+   a -= 8; // o mesmo que:
+   //b = b - 8; b -= 8;
+
+   printf("O valor de b eh: %d\n", b);
+
+   return 0;
+}
