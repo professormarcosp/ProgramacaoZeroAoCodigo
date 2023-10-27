1187c880def1c0942d1515f007f194f53ea14b0c
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2023-10-27 15:35:58 -0300

    Create exercicio03.c

---
+++ operadores/simplificados/exercicio03.c
@@ -0,0 +1,19 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+   int c = 15;
+
+   c *= 3; // o mesmo que: c = c * 3; c *= 3;
+
+   printf("O valor de c eh: %d\n", c);
+
+   return 0;
+}
