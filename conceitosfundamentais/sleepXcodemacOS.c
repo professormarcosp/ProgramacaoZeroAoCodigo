cc155465aeb2fc86939444f11e09bc258ad6a0c2
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-01-18 15:48:25 -0300

    Create sleepXcodemacOS.c

---
+++ conceitosfundamentais/sleepXcodemacOS.c
@@ -0,0 +1,19 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv)
+{
+   printf("Esperando por 5 Segundos...\n");
+   Sleep(5);
+   printf("Continuando a execução do programa. \n");
+  
+
+   return 0;
+}
