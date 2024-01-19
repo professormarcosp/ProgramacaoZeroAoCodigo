69e3e6be103e710867966e49f24500c43e548605
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-01-19 16:05:55 -0300

    Create restodadivisao01.c

---
+++ operadores/mod/restodadivisao01.c
@@ -0,0 +1,23 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+   int resultado;
+
+   //Exemplo 1: Obtendo o resto da divisão de 10 por 3
+   resultado = 10 % 3;
+   printf("O resto da divisao de 10 por 3: %d\n", resultado);
+
+   //Exemplo 2 Obtendo o resto da divisão de 15 por 7
+   resultado = 15 % 6;
+   printf("O resto da divisao de 15 por 7: %d\n", resultado);
+
+   return 0;
+}
