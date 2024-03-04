7c7003be2e89d665ea22362f3bfd67451ccc6eab
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-03-04 10:43:37 -0300

    Create exercicio_ASCII.c

---
+++ variaveis/ExercicioASCII/exercicio_ASCII.c
@@ -0,0 +1,18 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+*/
+
+//Fulano Beltrano
+
+#include <stdio.h>
+
+int main()
+{
+   printf("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",
+          0b1000110,0b1110101,0b1101100,0b1100001,0b1101110,0b1101111,0b100000,
+0b1000010, 0b1100101,0b1101100, 0b1110100, 0b1110010, 0b1100001, 0b1101110, 0b1101111);
+
+
+   return 0;
+}
