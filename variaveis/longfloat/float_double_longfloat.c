9707d04570b75c6d7057a8f4cb2c274e23819692
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-03-04 10:51:56 -0300

    Update float_double_longfloat.c

--- variaveis/longfloat/float_double_longfloat.c
+++ variaveis/longfloat/float_double_longfloat.c
@@ -5,6 +5,15 @@ Aprendendo Programação do Zero ao Código com a Linguagem C
 
 #define __USE_MINGW_ANSI_STDIO 1
 
+/*A diretiva #define __USE_MINGW_ANSI_STDIO 1 é uma forma de definir explicitamente a macro
+__USE_MINGW_ANSI_STDIO como 1. Isso basicamente ativa a funcionalidade associada a essa macro.
+No caso específico do MinGW, essa macro é usada para indicar que o código deve usar as funções 
+de entrada e saída padrão do padrão ANSI C, conforme definido pela ANSI. Por padrão, o MinGW 
+pode usar uma implementação própria dessas funções, que pode não ser totalmente compatível com 
+o padrão ANSI C. Ao definir essa macro como 1, você está instruindo o MinGW a usar as funções 
+de entrada e saída conforme definido pelo padrão ANSI C.
+*/
+
 #include <stdio.h>
 
 
