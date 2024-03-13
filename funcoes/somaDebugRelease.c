41e57ee57f4fe5f7ddbc01301054721d1fc9f377
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-03-13 08:26:34 -0300

    Create somaDebugRelease.c

---
+++ funcoes/somaDebugRelease.c
@@ -0,0 +1,16 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int a = 5;
+  int b = 10;
+  int soma = a + b;
+  printf ("Soma: %d\n, soma);
+  return 0;
+}
