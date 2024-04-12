1b0e80675a6f7d5fc553b2f60e27dd6c0cdd725f
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-04-12 14:24:57 -0300

    Update ExercicioOperadorTernario02.c

--- OperadorTernario/ExercicioOperadorTernario02.c
+++ OperadorTernario/ExercicioOperadorTernario02.c
@@ -1,36 +1,26 @@
-/*Professor Marcos Pacheco
-professormarcosp@gmail.com
+/* Professor Marcos Pacheco professormarcosp@gmail.com
 Aprendendo Programação do Zero ao Código com a Linguagem C
-Meus cursos em: https://www.udemy.com/user/marcostim/
-*/
+Meus cursos em: https://www.udemy.com/user/marcostim/ */
 
 #include <stdio.h>
 
-int verifica_maior(int x, int y);
-
+int verifica_maior(int x, int y)
+{
+   //se for maior que y(true) retorne x
+   //senão se x não for maior que y(false) retorne y
+   return (x > y) ? x : y;
+}
 
 int main()
 {
-   int x, y;
-   int maior_numero = 0;
+   int num1, num2;
 
-   printf("\nDigite o valor de x: ");
-   scanf("%d", &x);
+   printf("\nDigite o valor do primeiro numero: ");scanf("%d", &num1);
+   printf("\nDigite o valor do segundo numero: "); scanf("%d", &num2);
 
-   printf("\nDigite o valor de y: ");
-   scanf("%d", &y);
+   int maior_numero = verifica_maior(num1, num2);
 
-   maior_numero = verifica_maior(x, y);
-
-   printf("O Maior numero eh: %d\n", maior_numero);
+   printf("\nO maior numero digitado foi: %d\n", maior_numero);
 
    return 0;
-}
-
-int verifica_maior(int x, int y)
-{
-   // se x for maior que y(true) retorne x
-   //senão se x não for maior(false) que y retorne y
-   return (x > y) ? x : y;
-
 }
