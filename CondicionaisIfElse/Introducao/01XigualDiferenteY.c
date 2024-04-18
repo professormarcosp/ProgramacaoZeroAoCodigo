a743a8940d0c86bf656ce7f77ba14fdd9cb7cf16
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-04-18 11:07:01 -0300

    Create 01XigualDiferenteY.c

---
+++ CondicionaisIfElse/Introducao/01XigualDiferenteY.c
@@ -0,0 +1,24 @@
+/* Professor Marcos Pacheco professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/ */
+
+#include <stdio.h>
+
+
+int main()
+{
+   int x = 20;
+   int y = 10;
+
+   if(x == y)  // se x for igual a y
+   {
+      printf("x eh igual a y!\n");
+   }
+
+   else //senão
+   {
+      printf("x eh diferente de y\n");
+   }
+
+   return 0;
+}
