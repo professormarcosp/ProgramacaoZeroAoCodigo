4b2e7518a7efb3a98ab59d80f4701e4df77dc522
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-08 16:27:53 -0300

    Create HelloWorldWindows.c

---
+++ inicio/HelloWorldWindows.c
@@ -0,0 +1,20 @@
+/*
+Professor Marcos Pacheco
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+professormarcosp@gmail.com
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	printf("Hello World! \n");
+	system("Pause > Nul");
+	
+	return 0;
+}
