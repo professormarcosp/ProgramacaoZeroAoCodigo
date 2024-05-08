cd00390280a5f93153870fac15db64e405c048aa
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-08 16:18:43 -0300

    Create Primeiro.c

---
+++ inicio/Primeiro.c
@@ -0,0 +1,17 @@
+/*Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+professormarcosp@gmail.com
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	printf("Primeiro Programa \n"); 
+	system("PAUSE");
+	return 0;
+}
