48d007cf84019c2c7fbda51e6e38093114047a21
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-08 16:38:32 -0300

    Create HelloWorldLinux02.c

---
+++ inicio/HelloWorldLinux02.c
@@ -0,0 +1,19 @@
+/*
+Professor Marcos Pacheco
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+professormarcosp@gmail.com
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+printf("Primeiro Programa\n");
+fflush(stdout); // Limpa o Buffer de Saída do stream stdout
+system( "read -p 'Pressione <ENTER> para continuar... ' echo");
+
+return 0;
+}
