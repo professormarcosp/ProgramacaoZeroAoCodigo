068d6bd8edeb22b5d837f0915ee85a44ea5fdeff
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-08 16:34:10 -0300

    Create HelloWorldLinux01.c

---
+++ inicio/HelloWorldLinux01.c
@@ -0,0 +1,19 @@
+/*
+Professor Marcos Pacheco
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+professormarcosp@gmail.com
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  
+  printf("Primeiro Programa\n");
+  printf("Pressione Qualquer Tecla para Continuar\n")
+  getchar();
+
+  return 0;
+}
