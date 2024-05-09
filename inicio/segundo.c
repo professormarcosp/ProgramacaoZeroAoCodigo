00d8c43fdb8fda4d1dfb4536d37e80c8e5e2d5d9
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-09 09:02:20 -0300

    Create segundo.c

---
+++ inicio/segundo.c
@@ -0,0 +1,18 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Acesse meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   printf("Oi novamente!\n");
+
+   system("PAUSE");
+   return 0;
+}
