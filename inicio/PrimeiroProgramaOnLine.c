245b48ba29fd9f2949bf5ccf45b1e2452208b738
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-09 09:15:44 -0300

    Create PrimeiroProgramaOnLine.c

---
+++ inicio/PrimeiroProgramaOnLine.c
@@ -0,0 +1,20 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
+/***********************************
+Compilador on-line
+Exemplos: Professor Marcos Pacheco
+**************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   printf("Primeiro Programa On-Line\n");
+   return 0;
+}
