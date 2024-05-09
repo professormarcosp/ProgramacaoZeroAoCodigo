77d2b8bf5468d0cb78199254d09c9d45e4507a37
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-09 10:07:25 -0300

    Update declarando_variaveis01.c

--- variaveis/declarando_variaveis01.c
+++ variaveis/declarando_variaveis01.c
@@ -1,20 +1,28 @@
-/*
-Professor Marcos Pacheco
-professormarcosp@gmail.com
-Aprendendo Programação do Zero ao Código com a Linguagem C
-Acesse aqui meus cursos em: https://www.udemy.com/user/marcostim/
+/*Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
 https://linkedin.com/in/professor-marcos-pacheco/
+professormarcosp@gmail.com
 */
+
+/*Crie um programa em linguagem C que declara as seguintes variáveis:
+Uma variável do tipo inteiro de nome numero
+Uma variável do tipo float de nome salario
+Uma variável do tipo double de nome consumo_total
+Uma variável do tipo booleano de nome sinal_ativado*/
 
 #include <stdio.h>
 
+
 int main()
 {
-
-   int numero; //%d para inteiros
-   float salario; //%f para float
-   double consumo_total; //%lf para double obs: lf = long float ou float longo
-   _Bool sinal_ativado;//%d pois será 0 ou 1 (inteiro)
+   int numero;  //int número inteiros
+   float salario;  // float números ponto flutuantes ou números reais com vírgula (.ponto)
+   double consumo_total;// double, é um float com maior precisão, com dupla precisão
+   //consegue armazenar números bem maiores, com maiores dígitos
+   _Bool sinal_ativado; // booleano estados verdadeiro (true) e falso (false)
+   //sendo que 0(zero) é falso e 1(um)é considerado verdadeiro em C
+   //na realidade 0 é falso e qualquer coisa diferente de zero é considerado verdadeiro em C
 
    getchar();
    return 0;
