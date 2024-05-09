d9dec07178f5304b6059cd87200b225c576c675e
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-09 10:03:15 -0300

    Create declarando_variaveis01.c

---
+++ variaveis/declarando_variaveis01.c
@@ -0,0 +1,21 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Acesse aqui meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
+#include <stdio.h>
+
+int main()
+{
+
+   int numero; //%d para inteiros
+   float salario; //%f para float
+   double consumo_total; //%lf para double obs: lf = long float ou float longo
+   _Bool sinal_ativado;//%d pois será 0 ou 1 (inteiro)
+
+   getchar();
+   return 0;
+}
