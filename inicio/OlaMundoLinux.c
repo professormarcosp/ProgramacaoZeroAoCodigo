661935733d263ce8604785193e2b1a8202b0c86a
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-09 09:10:11 -0300

    Create OlaMundoLinux.c

---
+++ inicio/OlaMundoLinux.c
@@ -0,0 +1,17 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Acesse aqui meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Olá Mundo\n");
+  return 0;
+}
+
+
