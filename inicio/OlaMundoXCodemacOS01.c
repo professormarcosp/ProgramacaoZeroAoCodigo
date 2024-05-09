a3c4a226fb36ccbd83badc4f0041fcf225ac1ea8
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-09 08:32:21 -0300

    Create OlaMundoXCodemacOS01.c

---
+++ inicio/OlaMundoXCodemacOS01.c
@@ -0,0 +1,17 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
+/*Criando e Compilando Programa em C com Xcode no mac0S*/
+
+#include <stdio.h>
+
+int main()
+{
+   printf("Olá Mundo\n");
+   return 0;
+}
