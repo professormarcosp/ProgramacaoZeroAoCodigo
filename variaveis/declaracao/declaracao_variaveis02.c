8ac8a1bf6c0c8356e92aedd6313f3dc6791d95de
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-09 10:09:46 -0300

    Create declaracao_variaveis02.c

---
+++ variaveis/declaracao/declaracao_variaveis02.c
@@ -0,0 +1,23 @@
+/*Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+professormarcosp@gmail.com
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+   int numero = 109;  //int número inteiros
+   float salario = 5689.23;  // float números ponto flutuantes ou números reais com vírgula (.ponto)
+   double consumo_total 157689.89;// double, é um float com maior precisão, com dupla precisão
+   //consegue armazenar números bem maiores, com maiores dígitos
+   _Bool sinal_ativado = 1; // booleano estados verdadeiro (true) e falso (false)
+   //sendo que 0(zero) é falso e 1(um)é considerado verdadeiro em C
+   //na realidade 0 é falso e qualquer coisa diferente de zero é considerado verdadeiro em C
+
+   getchar();
+   return 0;
+}
