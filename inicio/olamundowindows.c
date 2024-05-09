a9e017d82b155792e829fb0236088f4e24634df3
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-09 09:30:17 -0300

    Create olamundowindows.c

---
+++ inicio/olamundowindows.c
@@ -0,0 +1,23 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
+//Projeto: Hello World!
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main()
+{
+
+   setlocale(LC_ALL, "");
+	printf("Olá Mundo!");
+
+	system("PAUSE > nul");
+	return 0;
+}
