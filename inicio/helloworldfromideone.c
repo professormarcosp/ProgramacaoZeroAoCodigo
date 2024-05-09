cd76ce24d5797ac3f596fbc6d74d471db2a5f927
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-09 09:26:59 -0300

    Create helloworldfromideone.c

---
+++ inicio/helloworldfromideone.c
@@ -0,0 +1,20 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
+/***********************************
+Professor Marcos Pacheco
+Outro Exemplo de Compilação On-line
+**************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   printf("Hello World from ideone!");
+   return 0;
+}
