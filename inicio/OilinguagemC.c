e417157d816f2988581f1281b270886d07f3846a
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-13 16:12:30 -0300

    Create OilinguagemC.c

---
+++ inicio/OilinguagemC.c
@@ -0,0 +1,16 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Acesse meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+   printf(\nOi Limguagem C!!!");
+   return 0;
+}
