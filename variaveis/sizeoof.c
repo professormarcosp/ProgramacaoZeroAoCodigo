5c5bd21515cec6edbbf24819962287fc40853e75
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-13 16:29:49 -0300

    Create sizeoof.c

---
+++ variaveis/sizeoof.c
@@ -0,0 +1,25 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Acesse meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
+#include <stdio.h>
+
+int main() {
+    int x;
+    double y;
+    char z;
+
+    printf("Tamanho de int: %d bytes\n", sizeof(int));
+    printf("Tamanho de double: %d bytes\n", sizeof(double));
+    printf("Tamanho de char: %d bytes\n", sizeof(char));
+
+    printf("Tamanho de x: %d bytes\n", sizeof(x));
+    printf("Tamanho de y: %d bytes\n", sizeof(y));
+    printf("Tamanho de z: %d bytes\n", sizeof(z));
+
+    return 0;
+}
