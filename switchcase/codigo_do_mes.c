3e1a74d550acaff8931c860e290ca422bdd29427
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-15 10:17:37 -0300

    Create codigo_do_mes.c

---
+++ switchcase/codigo_do_mes.c
@@ -0,0 +1,63 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Acesse aqui meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int mes;
+
+    printf("\nDigite o numero do mes (1 a 12): ");
+    scanf("%d", &mes);
+
+    switch (mes)
+    {
+        case 1:
+            printf("\nJaneiro\n");
+            break;
+        case 2:
+            printf("\nFevereiro\n");
+            break;
+        case 3:
+            printf("\nMarco\n");
+            break;
+        case 4:
+            printf("\nAbril\n");
+            break;
+        case 5:
+            printf("\nMaio\n");
+            break;
+        case 6:
+            printf("\nJunho\n");
+            break;
+        case 7:
+            printf("\nJulho\n");
+            break;
+        case 8:
+            printf("\nAgosto\n");
+            break;
+        case 9:
+            printf("\nSetembro\n");
+            break;
+        case 10:
+            printf("\nOutubro\n");
+            break;
+        case 11:
+            printf("\nNovembro\n");
+            break;
+        case 12:
+            printf("\nDezembro\n");
+            break;
+
+        default:
+            printf("\nMes invalido\n");
+    }
+
+    return 0;
+}
+
