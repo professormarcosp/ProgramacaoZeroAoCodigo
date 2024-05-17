a68041fefaa398ee87a956ef6187b9aa247a3356
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-17 14:55:49 -0300

    Create DiaDaSemanaIfelse.c

---
+++ CondicionaisIfElse/Introducao/DiaDaSemanaIfelse.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main() 
+{
+    int dia;
+    printf("Digite um numero de 1 a 7: ");
+    scanf("%d", &dia);
+
+    if (dia == 1)
+    {
+        printf("Domingo\n");
+    }
+    else if (dia == 2)
+    {
+        printf("Segunda-feira\n");
+    }
+    else if (dia == 3)
+    {
+        printf("Terça-feira\n");
+    }
+    else if (dia == 4)
+    {
+        printf("Quarta-feira\n");
+    }
+    else if (dia == 5)
+    {
+        printf("Quinta-feira\n");
+    }
+    else if (dia == 6)
+    {
+        printf("Sexta-feira\n");
+    }
+    else if (dia == 7)
+    {
+        printf("Sabado\n");
+    }
+    else
+    {
+        printf("Numero invalido\n");
+    }
+
+    return 0;
+}
