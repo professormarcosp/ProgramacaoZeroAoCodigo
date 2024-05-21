2566455c310401b55f5325820cbf9b003d54e944
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-05-21 14:23:52 -0300

    Update categoria_produto_preco.c

--- CondicionaisIfElse/exercicios/categoria_produto_preco.c
+++ CondicionaisIfElse/exercicios/categoria_produto_preco.c
@@ -1,3 +1,11 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Acesse aqui meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
 #include <stdio.h>
 
 int main() 
