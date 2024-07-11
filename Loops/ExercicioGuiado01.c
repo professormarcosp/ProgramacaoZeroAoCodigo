6eacf847682a29bf5f58c01bd6e16dbe08ee4e32
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-07-11 16:17:52 -0300

    Create ExercicioGuiado01.c

---
+++ Loops/ExercicioGuiado01.c
@@ -0,0 +1,20 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    
+    while(i <= 5)
+    {
+        printf("%d\n", i);
+        i++;  //importante!
+    }
+
+    return 0;
+}
