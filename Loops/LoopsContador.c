de51d31023240a2a7f1810f390363b977496efd3
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-07-11 16:14:07 -0300

    Create LoopsContador.c

---
+++ Loops/LoopsContador.c
@@ -0,0 +1,23 @@
+/*
+Professor Marcos Pacheco
+professormarcosp@gmail.com
+Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+Acesse aqui meus cursos em: https://www.udemy.com/user/marcostim/
+https://linkedin.com/in/professor-marcos-pacheco/
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int contador = 1;
+
+    while (contador <= 5)
+    {
+        printf("Esta eh a iteracao %d\n", contador);
+        contador++;
+    }
+
+    return 0;
+}
