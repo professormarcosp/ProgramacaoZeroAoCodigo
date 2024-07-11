0a0a45b52d9a8279b49ed006991bcc79bd6e5aa4
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-07-11 16:30:44 -0300

    Create ContagemRegressiva.c

---
+++ Loops/ContagemRegressiva.c
@@ -0,0 +1,21 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    int contador = 5;
+
+    while (contador >= 1)
+    {
+        printf("%d ", contador);
+        contador--;
+    }
+
+    return 0;
+}
