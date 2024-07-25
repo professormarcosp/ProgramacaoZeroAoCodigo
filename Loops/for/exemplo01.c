d2036e02b5b9fbdf03ca47b347e42a9316982688
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-07-25 14:33:41 -0300

    Create exemplo01.c

---
+++ Loops/for/exemplo01.c
@@ -0,0 +1,19 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+    int i;
+    for (i = 0; i < 10; i++) 
+    {
+         printf("\nValor de i: %d", i);
+    }
+
+    return 0;
+    
+}
