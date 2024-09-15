ce43c26df3f7f43003ad10d67825dba7a5caa820
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-09-15 18:07:58 -0300

    Update MultiplasVariaveisFor.c

--- Loops/for/multiplasvariavies/MultiplasVariaveisFor.c
+++ Loops/for/multiplasvariavies/MultiplasVariaveisFor.c
@@ -1,8 +1,8 @@
 /*
 Professor Marcos Pacheco
 Curso: Aprendendo Programação do Zero ao Código com a Linguagem C*/
-[Disponível em: Udemy](https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244)
-
+Disponível em: https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
 
 #include <stdio.h>
 
