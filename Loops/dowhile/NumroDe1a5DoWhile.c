4adf4af4f652e0094ef10a659b76aece63897e98
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2024-09-17 16:49:26 -0300

    Create NumroDe1a5DoWhile.c

---
+++ Loops/dowhile/NumroDe1a5DoWhile.c
@@ -0,0 +1,22 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+
+    do //Faça
+    {
+        printf("%d ", i);
+        i++;
+
+    } while (i <= 5); //Equanto i for menor ou igual a 5
+
+    return 0;
+}
