6dac3689781ed3052c4d96c6363abdb0d3dbd464
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-04-01 11:21:13 -0300

    Create MotivacaoVetores.c

---
+++ VetoresArraysMatrizes/MotivacaoVetores.c
@@ -0,0 +1,42 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int idade1, idade2, idade3, idade4, idade5;
+    int idade6, idade7, idade8, idade9, idade10;
+
+    //atribuindo valores as variáveis
+    idade1 = 25;
+    idade2 = 30;
+    idade3 = 20;
+    idade4 = 35;
+    idade5 = 28;
+    idade6 = 40;
+    idade7 = 18;
+    idade8 = 0;
+    idade9 = 6;
+    idade10 = 4;
+
+    //exibindo os valores
+    printf("\nIdade 1: %d" , idade1);
+    printf("\nIdade 2: %d" , idade2);
+    printf("\nIdade 3: %d" , idade3);
+    printf("\nIdade 4: %d" , idade4);
+    printf("\nIdade 5: %d" , idade5);
+    printf("\nIdade 6: %d" , idade6);
+    printf("\nIdade 7: %d" , idade7);
+    printf("\nIdade 8: %d" , idade8);
+    printf("\nIdade 9: %d" , idade9);
+    printf("\nIdade 10: %d" , idade10);
+
+    
+    
+    return 0;
+}
