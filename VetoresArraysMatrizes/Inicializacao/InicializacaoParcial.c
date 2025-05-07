e3b46e2ae2571b5011eba5f67310a65d483f18d0
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-05-07 14:30:59 -0300

    Create InicializacaoParcial.c

---
+++ VetoresArraysMatrizes/Inicializacao/InicializacaoParcial.c
@@ -0,0 +1,20 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int numeros[5] = {10, 20};
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\nElemento %d: %d", i, numeros[i]);
+    }
+
+    return 0;
+}
