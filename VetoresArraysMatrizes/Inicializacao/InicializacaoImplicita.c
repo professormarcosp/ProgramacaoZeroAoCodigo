358c1fcdf954738ac52ebdda9f4c42b533989f9b
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-05-07 14:43:24 -0300

    Create InicializacaoImplicita.c

---
+++ VetoresArraysMatrizes/Inicializacao/InicializacaoImplicita.c
@@ -0,0 +1,20 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int numeros [] = {5, 15, 25, 35};
+
+    for(int i = 0; i < 4; i++)
+    {
+        printf("\nElemento %d: %d", i , numeros[i]);
+    }
+
+    return 0;
+}
