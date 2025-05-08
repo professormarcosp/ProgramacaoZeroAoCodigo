fefcba90f639c29fd26764c11d390a434525dc9b
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-05-08 11:44:16 -0300

    Create iterandocomdowhile.c

---
+++ VetoresArraysMatrizes/iterando/iterandocomdowhile.c
@@ -0,0 +1,24 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int numeros[5] = {1, 2, 3, 4, 5};
+    int i = 0; //variavel controle do loop para iterar pelos array
+
+    printf("\nIterando vetor com while");
+    do 
+    {
+        printf("\nElemento %d: %d", i, numeros[i]);
+        i++; // incremento
+    } while (i < 5); // condicao de parada, poderia ser i <= 4 pois último índice é 4
+
+
+    return 0;
+}
