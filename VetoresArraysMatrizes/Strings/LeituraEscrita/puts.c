8d9ff3065a57a6a8a0c68b1f7cd777c425edcd45
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-06-18 16:31:14 -0300

    Create puts.c

---
+++ VetoresArraysMatrizes/Strings/LeituraEscrita/puts.c
@@ -0,0 +1,18 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    char nome[] = "Professor Marcos Pacheco";
+
+    puts(nome);
+
+    return 0;
+}
