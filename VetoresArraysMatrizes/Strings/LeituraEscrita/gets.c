98e01269ec06b4706b697acd8908942f74d35249
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-06-18 16:18:36 -0300

    Create gets.c

---
+++ VetoresArraysMatrizes/Strings/LeituraEscrita/gets.c
@@ -0,0 +1,21 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+
+#include <stdio.h>
+
+int main()
+{
+    char nome[50];
+
+    printf("Digite seu nome: ");
+    gets(nome);
+
+    printf("Nome digitado: %s\n", nome);
+    
+    return 0;
+}
