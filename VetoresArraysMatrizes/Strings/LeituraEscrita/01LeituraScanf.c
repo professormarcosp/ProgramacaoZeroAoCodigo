b24b565e971f92f6db33786c92e4b8aac289a14b
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-06-18 14:55:53 -0300

    Create 01LeituraScanf.c

---
+++ VetoresArraysMatrizes/Strings/LeituraEscrita/01LeituraScanf.c
@@ -0,0 +1,19 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D
+
+#include <stdio.h>
+
+int main()
+{
+    char nome[50];
+
+    printf("\nDigite seu nome: ");
+    scanf("%s", nome); //lembre-se string é vetor
+    //e vetor não coloca &nome apenas o nome do vetor!
+    printf("Nome Digitado: %s\n", nome);
+
+    return 0;
+}
