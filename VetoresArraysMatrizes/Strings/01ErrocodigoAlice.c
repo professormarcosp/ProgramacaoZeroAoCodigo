5a4087860d0e680e2dbd8419500d3b1d7cededee
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-06-18 10:41:16 -0300

    Create 01ErrocodigoAlice.c

---
+++ VetoresArraysMatrizes/Strings/01ErrocodigoAlice.c
@@ -0,0 +1,18 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    //char nome[10] = "Alice";
+    char nome[10] = {'A', 'l', 'i', 'c', 'e', '\0'};
+    
+    printf("\nNome: %s", nome);
+
+    return 0;
+}
