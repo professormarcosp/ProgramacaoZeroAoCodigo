1d0869231892d7cd0450a90ad545de64eb87a16c
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-06-25 15:43:07 -0300

    Update strcmp2.c

--- VetoresArraysMatrizes/strings/strcmp2.c
+++ VetoresArraysMatrizes/strings/strcmp2.c
@@ -2,7 +2,7 @@
 Professor Marcos Pacheco
 Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
 Disponível em: 
-https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+[Udemy - Curso de Linguagem C](https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244)
 */
 
 
