8d4907a2b0763003135f6ce1642edd5a534d845a
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-06-25 16:19:32 -0300

    Create exercicio01.c

---
+++ VetoresArraysMatrizes/strings/exercicios/exercicio01.c
@@ -0,0 +1,20 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    char nome[50];
+
+    printf("Digite seu nome: ");
+    fgets(nome, 50, stdin);
+
+    printf("Nome Digitado: %s", nome);
+
+    return 0;
+}
