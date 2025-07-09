004da637881b77e13b102080b757cbd8eca9234f
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-07-09 13:08:11 -0300

    Create SomaValorDigitadoAosElementosDoVetor.c

---
+++ VetoresArraysMatrizes/sizeof/SomaValorDigitadoAosElementosDoVetor.c
@@ -0,0 +1,46 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void somar_valor(int vetor[], int valor);
+
+int main()
+{
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+
+    int numeros[] = {1, 2, 3, 4, 5};
+    int valor_soma;
+
+    // Solicitando ao usuario o valor a ser mudado
+    printf("\nDigite um valor para somar a cada elemento do vetor: ");
+    scanf("%d", &valor_soma);
+
+    // chamada da função para modificar o valor
+    somar_valor(numeros, valor_soma);
+
+    printf("\nVetor após a soma:\n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d ", numeros[i]);
+    }
+
+    return 0;
+}
+
+void somar_valor(int vetor[], int valor)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        vetor[i] = vetor[i] + valor;
+    }
+}
