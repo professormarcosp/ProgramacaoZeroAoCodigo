b57644587b77e5a817e010d3faa1dd7d20234fa6
professormarcosp <42651426+professormarcosp@users.noreply.github.com>
2025-09-04 10:31:03 -0300

    Create EspacoDireiraEsquerda.c

---
+++ VetoresArraysMatrizes/Strings/LeituraEscrita/EspacoDireiraEsquerda.c
@@ -0,0 +1,18 @@
+/*
+Professor Marcos Pacheco
+Curso: Aprendendo Programação do Zero ao Código com a Linguagem C
+Disponível em: 
+https://www.udemy.com/course/aprendendo-programacao-do-zero-ao-codigo-com-a-linguagem-c/?referralCode=B25C90BF63C49D7D9244
+*/
+
+#include <stdio.h>
+
+int main()
+{
+      printf( "%10s", "Texto"); 
+      printf("%s", "Depois de 10s\n");
+      printf( "%-10s", "Texto");
+      printf("%s", "Depois de -10s");
+
+    return 0;
+}
